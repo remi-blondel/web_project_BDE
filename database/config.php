b8b4a9d996a27d91a76ccb2116606bb43f416201
RuskofFR <florian.bobin@viacesi.fr>
2017-04-19 16:29:41 +0200

    rr

--- database/config.php
+++ database/config.php
@@ -1,5 +1,5 @@
 <?php
   require_once 'singleton.php';
 
-  singleton::setConfig('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', '');
+  singleton::setConfig('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', 'toor');
 ?>
