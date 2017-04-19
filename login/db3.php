6602177bbb7de5516c435a1eb7cbe4fb9aac4695
Thorn485 <Nicolas@MacBook-Pro-de-Nicolas-2.local>
2017-04-19 11:34:01 +0200

    Modification du traitement de l’inscription

--- login/db3.php
+++ login/db3.php
@@ -23,32 +23,48 @@ $email1=$_POST['email'];
 $password1=$_POST['pass2'];
 $password2=$_POST['pass3'];
 
-if ($password1 == $password2) {
-
-	echo 'Les informations que vous avez rentré sont corretes et sont enregistrés! Redirection vers la page de connexion';
+if (preg_match("/.*@viacesi\.fr/", $email1) OR preg_match("/.*@cesi\.fr/", $email1))  
 
 
+	{
 
 
-$req = $bdd->prepare('INSERT INTO Membres_du_site(Prénom, Nom, Email, Mot_de_Passe) VALUES(:prenom, :nom, :email, :mot_de_passe)');
-$req->execute(array(
-	
-	'prenom' => $firstname1,
-	'nom' => $lastname1,
-	'email' => $email1,
-	'mot_de_passe' => $password1
-	));
+		if ($password1 == $password2)
+
+		{
+
+
+		$req = $bdd->prepare('INSERT INTO Membres_du_site(Prénom, Nom, Email, Mot_de_Passe) VALUES(:prenom, :nom, :email, :mot_de_passe)');
+		$req->execute(array(
+		
+		'prenom' => $firstname1,
+		'nom' => $lastname1,
+		'email' => $email1,
+		'mot_de_passe' => $password1
+		));
+
+		echo 'Les informations que vous avez rentré sont corretes et sont enregistrées! Redirection vers la page de connexion...';
+
+
+		}
+
+		else
+
+		{
+			echo 'Les mots de passes saisis sont différents. Veuillez remplir de nouveau le formulaire. Redirection vers la page de connexion...';
+		}
 
 
 
-}
+	}
 
 else
 
-{
+	{
+			echo 'L\'adresse email saisie n\'est pas valide! Redirection vers la page de connexion...';
 
-	echo 'les mots de passes sont différents. Veuillez remplir de nouveau le formulaire. Redirection vers la page de connexion';
-}
+	}
+
 
 
 //closecursor?
