1c11986837230f882b5e60d0aa72b5dfe6577c28
Thorn485 <Nicolas@MacBook-Pro-de-Nicolas-2.local>
2017-04-20 09:37:06 +0200

    a

--- cible.php
+++ cible.php
@@ -8,22 +8,35 @@ require_once 'database/config.php';
 //header("Refresh: 5; url=Login.html");
 
 
-$email1=$_POST['email1'];
-$password1=sha1($_POST['pass']);
+$email3=$_POST['email1'];
+$password3=sha1($_POST['pass']);
 
 
 $req = singleton::getInstance()->prepare('SELECT pk_id_user FROM user WHERE email = :email AND pass = :pass');
 $req->execute(array(
-    'email' => $email1,
-    'pass' => $password1));
+    'email' => $email3,
+    'pass' => $password3));
 
 $resultat = $req->fetch();
 
-echo $donnees['pk_id_user'] . '<br />';
+echo $resultat['pk_id_user'] . '<br />';
 
+if (!$resultat)
+{
 
+echo 'Mauvais identifiant ou mot de passe !';
 
+}
 
+else
 
+{	
+
+session_start();
+    $_SESSION['pk_id_user'] = $resultat['pk_id_user'];
+    $_SESSION['email1'] = $email3;
+    echo 'Vous êtes connecté !';
+
+}
 
 ?>
